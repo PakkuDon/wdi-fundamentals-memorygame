fd8bc59f46fa2e70821f860dd2fd2b242c7f53f5
PakkuDon <pakku.pd@gmail.com>
2016-09-21 22:06:59 +1000

    Added result alert
    - Unit 9 Assignment completed
    - Selected cards currently hard coded

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,6 +4,14 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-// Equality checks
-console.log(cardOne == cardTwo);
-console.log(cardOne == cardFour);
+// Pick cards
+var firstPick = cardOne;
+var secondPick = cardTwo;
+
+// Display whether user selected cards are a match
+if (firstPick === secondPick) {
+  alert('You found a match!');
+}
+else {
+  alert('Sorry, try again.');
+}
