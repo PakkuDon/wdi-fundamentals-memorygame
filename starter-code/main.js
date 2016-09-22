19a39262ed44862d6a4f335f9a19f913dfd81559
PakkuDon <pakku.pd@gmail.com>
2016-09-23 00:01:29 +1000

    Cards now generated programmatically

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,9 +9,25 @@ var firstPick = cardOne;
 var secondPick = cardTwo;
 
 // Display whether user selected cards are a match
+/*
 if (firstPick === secondPick) {
   alert('You found a match!');
 }
 else {
   alert('Sorry, try again.');
+}
+*/
+
+// Construct board
+createCards();
+
+// Create HTML elements representing cards
+function createCards() {
+  var board = document.querySelector('#game-board');
+
+  for (var i = 0; i < 4; i++) {
+    var card = document.createElement('div');
+    card.className = 'card';
+    board.appendChild(card);
+  }
 }
