6d1e51d75dd6573407cc56813394cc37579f2175
PakkuDon <pakku.pd@gmail.com>
2016-09-23 15:25:00 +1000

    Bound card variables to DOM elements
    - Replaced cards with card array
    - Added separate functions for checking if two cards are selected
      and if both cards match
    - Added data-* attribute and event handlers to card elements

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,33 +1,46 @@
+var board = document.querySelector('#game-board');
+
 // Memory game cards
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
-
-// Pick cards
-var firstPick = cardOne;
-var secondPick = cardTwo;
-
-// Display whether user selected cards are a match
-/*
-if (firstPick === secondPick) {
-  alert('You found a match!');
-}
-else {
-  alert('Sorry, try again.');
-}
-*/
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 // Construct board
-createCards();
+createCards(board);
 
 // Create HTML elements representing cards
-function createCards() {
-  var board = document.querySelector('#game-board');
-
-  for (var i = 0; i < 4; i++) {
+function createCards(board) {
+  for (var i = 0; i < cards.length; i++) {
     var card = document.createElement('div');
+
+    // Assign attributes and event handlers
     card.className = 'card';
+    card.setAttribute('data-card', cards[i]);
+    card.addEventListener('click', isTwoCards);
+
+    // Add card to DOM
     board.appendChild(card);
+  }
+}
+
+// Check to see if there are cards in play
+function isTwoCards() {
+  // Add selected card to cards in play
+  cardsInPlay.push(this.getAttribute('data-card'));
+  
+  // If two cards in play check for a match
+  // and reset collection
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+// Notify user if selected cards match
+function isMatch(cardsInPlay) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!');
+  }
+  else {
+    alert('Sorry, try again.');
   }
 }
